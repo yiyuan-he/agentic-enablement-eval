f11fee2b89d9dd303b884f09c87a4ae7d716f90a
yiyuanh <yiyuanh@amazon.com>
2025-10-20 11:34:27 -0700

    add traffic generation to user data

--- infrastructure/ec2/terraform/user_data.sh
+++ infrastructure/ec2/terraform/user_data.sh
@@ -13,10 +13,21 @@ usermod -a -G docker ec2-user
 # Authenticate with ECR
 aws ecr get-login-password --region ${aws_region} | docker login --username AWS --password-stdin $(echo ${image_uri} | cut -d'/' -f1)
 
-# Run the application container
+# Pull Docker image
+docker pull ${image_uri}
+
+# Run container
 docker run -d --name ${app_name} \
   -p ${app_port}:${app_port} \
   -e PORT=${app_port} \
   -e SERVICE_NAME=${app_name} \
-  --restart unless-stopped \
+  -e AWS_REGION=${aws_region} \
   ${image_uri}
+
+# Wait for application to start
+sleep 10
+
+# Start traffic generator inside container
+docker exec -d ${app_name} bash /app/generate-traffic.sh
+
+echo "Application deployed and traffic generation started"
